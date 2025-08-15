727d613b29c3551c6c5d8d313b5024dac5fdc138
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-08-15 12:21:52 +0530

    math

---
+++ Math/math.js
@@ -0,0 +1,28 @@
+// console.log(parseInt(5/2));
+
+
+// console.log(Math.floor(9.99));
+// console.log(Math.ceil(9.01));
+
+// console.log(Math.round(9.47));
+
+// console.log(Math.trunc(9.99));
+
+// console.log(Math.max(5,10,25,20));
+// console.log(Math.min(5,10,2,20));
+
+// console.log(Math.abs(-56));
+
+// console.log(Math.sqrt(9));
+
+// console.log(Math.pow(2,3));
+
+
+// console.log(Math.floor(Math.random() * 10));
+
+console.log(Math.floor(Math.random() * 10000));
+
+
+
+
+
